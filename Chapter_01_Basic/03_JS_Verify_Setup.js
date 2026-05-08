0f0f7042c0a36ec7bf695070ff2fada8a51f3d3b
shaik jalal <you@example.com>
2026-05-09 00:06:59 +0530

    Add identifier examples with naming conventions

--- Chapter_01_Basic/03_JS_Verify_Setup.js
+++ Chapter_01_Basic/03_JS_Verify_Setup.js
@@ -1,3 +1,6 @@
 console.log(process.platform);
 console.log(process.arch);
 console.log("Node version: " + process.version);
+
+console.log("Hello, Node.js!");
+console.log("This is a simple Node.js application.");
