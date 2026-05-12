c73ff0de72e2e19ed0e55f21af9a842b924d5ed0
shaik jalal <you@example.com>
2026-05-13 01:40:24 +0530

    Commit latest code changes

---
+++ Chapter_04_Javascript_Conceptss/13_const_explained.js
@@ -0,0 +1,18 @@
+
+const BASE_URL = "https://app.thetestingacademy.com";
+// const BASE_URL = "https://app.thetestingacademy.com";
+//BASE_URL = "https:/ / staging.thetestingacademy.com";
+// TypeError: Assignment to constant variable.
+
+
+let name = "pending";
+name = "done";
+{
+    let name = "Jalal";
+}
+
+function say() {
+    let name = "Jalla";
+}
+say();
+say();
